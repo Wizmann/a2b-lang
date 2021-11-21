c4efbbc4c12daa329fa72bae16e96b9c00fa750d
Anonymous <anonymous@localhost>
2021-11-21 23:21:13 +0800

    Create A2B.py

---
+++ A2B.py
@@ -0,0 +1,177 @@
+#!/bin/python2
+#coding=utf-8
+import sys
+import re
+import argparse
+
+KEYWORD_ONCE = 'once'
+KEYWORD_START = 'start'
+KEYWORD_RETURN = 'return'
+KEYWORD_END = 'end'
+KEYWORD_NONE = None
+
+EXECUTED_NONE = 'none'
+EXECUTED_DONE = 'done'
+EXECUTED_RETURN = 'return'
+EXECUTED_PASS = 'pass'
+
+LINE_LENGTH_LIMIT = 512
+EXECUTOR_OPERATION_LIMIT = 1024
+
+class A2BParseException(Exception):
+    def __init__(self, line_no, description):
+        self.message = '[Syntax Error on L%d]: %s' % (line_no + 1, description)
+        super(A2BParseException, self).__init__(self.message)
+
+class A2BExecutionException(Exception):
+    def __init__(self):
+        self.message = '[Runtime Error]: %s' % (description)
+        super(A2BExecutionException, self).__init__(self.message)
+
+class Program(object):
+    def __init__(self, exprs):
+        self.exprs = []
+
+class Expression(object):
+    def __init__(self, line_no, plain_text, left, right):
+        self.line_no = line_no
+        self.plain_text = plain_text
+        self.left = left
+        self.right = right
+        self.executed = 0
+
+    def Execute(self, input_data):
+        if self.left.keyword == KEYWORD_ONCE and self.executed:
+            return EXECUTED_PASS, ''
+        if not self.left.match(input_data):
+            return EXECUTED_PASS, ''
+        self.executed += 1
+        result = self.left.replace(input_data, self.right)
+        if self.right.keyword == KEYWORD_RETURN:
+            return EXECUTED_RETURN, result
+        return EXECUTED_DONE, result
+
+class Pattern(object):
+    pattern_re = re.compile(r'^(\((?P<keyword>\w+?)\))?(?P<pattern>(((?![()^$=])[\x00-\x7F]))*)$')
+
+    def __init__(self, keyword, pattern):
+        self.keyword = keyword
+        self.pattern = pattern
+
+    def get_pattern(self):
+        if self.keyword == KEYWORD_START:
+            pattern = '^' + self.pattern
+        elif self.keyword == KEYWORD_END:
+            pattern = self.pattern + '$'
+        else:
+            pattern = self.pattern
+        return pattern
+
+    def match(self, input_data):
+        pattern = self.get_pattern()
+        return pattern in input_data
+
+    def replace(self, input_data, other):
+        pattern = self.get_pattern()
+        assert pattern in input_data
+
+        result = ''
+
+        if other.keyword == KEYWORD_RETURN:
+            result = other.pattern
+        elif other.keyword == KEYWORD_START:
+            result = input_data.replace(pattern, '', 1)
+            result = other.pattern + result
+        elif other.keyword == KEYWORD_END:
+            result = input_data.replace(pattern, '', 1)
+            result = result + other.pattern
+        else:
+            result = input_data.replace(pattern, other.pattern, 1)
+
+        result = result.replace('^', '').replace('$', '')
+        result = '^' + result
+        result = result + '$'
+
+        return result
+
+    @staticmethod
+    def Parse(line_no, input_data):
+        match = Pattern.pattern_re.search(input_data)
+        if not match:
+            raise A2BParseException(line_no, 'Invalid pattern "%s"' % input_data)
+        d = match.groupdict()
+        if not d:
+            raise A2BParseException(line_no, 'Invalid pattern "%s"' % input_data)
+        keyword = d.get('keyword', KEYWORD_NONE)
+        if keyword not in [KEYWORD_NONE, KEYWORD_START, KEYWORD_END, KEYWORD_RETURN, KEYWORD_ONCE]:
+            raise A2BParseException(line_no, 'Invalid keyword "(%s)"' % keyword)
+
+        return Pattern(keyword, d['pattern'])
+
+def parse(program):
+    lines = program.split("\n")
+
+    p = Program([])
+    for line_no, line in enumerate(lines):
+        if not line.strip():
+            continue
+        if line.count('=') != 1:
+            raise A2BParseException(line_no, 'Each line should have one and only one "="')
+        left, right = map(lambda s: Pattern.Parse(line_no, s.strip()), line.split('='))
+        if left.keyword == KEYWORD_RETURN:
+            raise A2BParseException(line_no, "Keyword(return) can't be placed on the left side of an expression")
+        if right.keyword == KEYWORD_ONCE:
+            raise A2BParseException(line_no, "Keyword(once) can't be placed on the right side of an expression")
+        e = Expression(line_no, line, left, right)
+        p.exprs.append(e)
+
+    return p
+
+def printable_format(line):
+    assert line.startswith('^')
+    assert line.endswith('$')
+    return line[1:-1]
+
+def execute(program, verbose=False):
+    line = '^' + raw_input().strip() + '$'
+    operation_counter = 0
+
+    while True:
+        for expr in program.exprs:
+            executed, output = expr.Execute(line)
+            operation_counter += 1
+            if executed == EXECUTED_RETURN:
+                return output
+            elif executed == EXECUTED_DONE:
+                if verbose:
+                    print >> sys.stderr, 'Step %d:'% operation_counter
+                    print >> sys.stderr, '  L%d: %s' % (expr.line_no, expr.plain_text)
+                    print >> sys.stderr, '>> %s' % printable_format(line)
+                    print >> sys.stderr, '<< %s' % printable_format(output)
+                    print >> sys.stderr, ''
+                line = output
+                break
+        else:
+            break
+        
+        if operation_counter > EXECUTOR_OPERATION_LIMIT:
+            raise A2BExecutionException("Time Limit Exceeded")
+
+        if len(line) > LINE_LENGTH_LIMIT:
+            raise A2BExecutionException("String Length Limit Exceeded")
+    return printable_format(line)
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description='A2B lang interpreter')
+    argparser.add_argument('-v', '--verbose', dest='verbose', action='store_true')
+    argparser.add_argument('filename')
+    argparser.set_defaults(verbose=False)
+    args = argparser.parse_args()
+
+    with open(args.filename) as input_file:
+        plain_text = input_file.read()
+        program = parse(plain_text)
+
+    result = execute(program, args.verbose)
+    print result
+
